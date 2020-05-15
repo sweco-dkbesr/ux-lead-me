a42433690858d28127f10d36df1e91b299e388d7
Bernhard Snizek <bernhard.snizek@sweco.dk>
2020-05-15 15:05:23 +0200

    Deploy to gh-pages

---
+++ precache-manifest.e26d9768380e25a10f79b800a5461873.js
@@ -0,0 +1,318 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5aa81859f5489281df637162bea05b5",
+    "url": "/content/home.html"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/content/how-far.html"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/content/hvor-langt-er-vi.html"
+  },
+  {
+    "revision": "df37837cf5cfe3cbd3d747470e105472",
+    "url": "/content/methods/3-12-1.html"
+  },
+  {
+    "revision": "be95d9f1d600ecfdb02060c0305d1c05",
+    "url": "/content/methods/design-sprint.html"
+  },
+  {
+    "revision": "391356062d481c83f49daa0fd7880b6a",
+    "url": "/content/methods/design-studio.html"
+  },
+  {
+    "revision": "69de56702c5a4db12400c40666739fdd",
+    "url": "/content/methods/design-thinking.html"
+  },
+  {
+    "revision": "81ac86b0f088a220963c13dcc8823ca4",
+    "url": "/content/methods/heuristic-evaluation.html"
+  },
+  {
+    "revision": "01e4e6f0040272aa98bf5e10927a7a36",
+    "url": "/content/methods/methods.html"
+  },
+  {
+    "revision": "fcb2786dcd364889936a46b03e7842a8",
+    "url": "/content/methods/ui.html"
+  },
+  {
+    "revision": "065673e42bb403c3656361c724142b37",
+    "url": "/content/methods/usability-interviews.html"
+  },
+  {
+    "revision": "26c2b529ddcdcd70a7c5ec38b06c3438",
+    "url": "/content/methods/usability-testing.html"
+  },
+  {
+    "revision": "d0e4a87935f1f91763bf4b6067ded477",
+    "url": "/content/methods/usability.html"
+  },
+  {
+    "revision": "e1cb9b74bbc68bf7476728e52b69b38c",
+    "url": "/content/methods/user-experience-design.html"
+  },
+  {
+    "revision": "2642f8b190b68f233586c0f1da00f80c",
+    "url": "/content/principles.html"
+  },
+  {
+    "revision": "4b8c8d9dfe4642dfcf148c6a10649f50",
+    "url": "/content/proces.html"
+  },
+  {
+    "revision": "4b5b7ac91f844298339a9bbcd8371f42",
+    "url": "/content/products/personas.html"
+  },
+  {
+    "revision": "fcb9edaf4b12c3a8c0d6ade9a72ea950",
+    "url": "/content/products/products.html"
+  },
+  {
+    "revision": "2979fe55046e301a03340fcabf10ff9a",
+    "url": "/content/products/protopersonas.html"
+  },
+  {
+    "revision": "cad2d7ad637b7fa663745a21f2ab6ff4",
+    "url": "/content/products/user-flows.html"
+  },
+  {
+    "revision": "09b51f972b45ca210e51804ffcb46ebf",
+    "url": "/content/tools/figma.html"
+  },
+  {
+    "revision": "aa0b3425d533977112cd2f53c7083dc8",
+    "url": "/content/tools/tools.html"
+  },
+  {
+    "revision": "68e615e59ab27df8f788f6c2cc4f7708",
+    "url": "/content/ux-and-you/product-owner.html"
+  },
+  {
+    "revision": "73db6e5deb0bc7c644cf7eec382f55f9",
+    "url": "/content/ux-and-you/ux-and-you-backup.html"
+  },
+  {
+    "revision": "595ab82b3b8aa2ebdac0bba6e1fd1882",
+    "url": "/content/ux-and-you/ux-and-you.html"
+  },
+  {
+    "revision": "0d90cf220a075c634c10cbe96a21f186",
+    "url": "/content/what.html"
+  },
+  {
+    "revision": "b5cdf6c638dfebcfeec3",
+    "url": "/css/app.3cb7b7ec.css"
+  },
+  {
+    "revision": "2ad1c2592c0fdd2641eb",
+    "url": "/css/chunk-vendors.62fe7e83.css"
+  },
+  {
+    "revision": "6d2c6f1b2a8acc089c4ae0cfc5fb7cc9",
+    "url": "/img/3-12-1.jpg"
+  },
+  {
+    "revision": "5df80f9eeca0882dbfe32de6922475fb",
+    "url": "/img/5-dimensions-of-usability.jpg"
+  },
+  {
+    "revision": "ce6ad83e4c27d876667b8f2b0bd61683",
+    "url": "/img/Untitled.sketch"
+  },
+  {
+    "revision": "a4ca1381df61fc88cf92989aeb43815b",
+    "url": "/img/design-sprint-book.jpg"
+  },
+  {
+    "revision": "5e33ac72f34bfb0bf52229788f7c01cd",
+    "url": "/img/design-sprint-flow.png"
+  },
+  {
+    "revision": "5ced044d599a0f9461413b7dcd66f262",
+    "url": "/img/design-sprint.jpg"
+  },
+  {
+    "revision": "e77c1abd5eb4fd3d7c460027270a7617",
+    "url": "/img/design-studio.png"
+  },
+  {
+    "revision": "dbd0c40eb43d63fbb53e7df611a27f9c",
+    "url": "/img/design-thinking.jpg"
+  },
+  {
+    "revision": "021317c0c60eac872378cf806c6b845a",
+    "url": "/img/download.png"
+  },
+  {
+    "revision": "36c3803b0773170fb94fb90268eae7a2",
+    "url": "/img/eye-tracking.png"
+  },
+  {
+    "revision": "b1057274624d8a4fd49cc9ea86f708b1",
+    "url": "/img/figma.jpg"
+  },
+  {
+    "revision": "de8b47dca6a0fb368aa5e6817935ee16",
+    "url": "/img/heuristic-evaluation.png"
+  },
+  {
+    "revision": "cb868e92b44de8ac0139920746a1196f",
+    "url": "/img/how-far.jpg"
+  },
+  {
+    "revision": "60c450759035592f2b16840a546bbbd3",
+    "url": "/img/i_want_you.jpg"
+  },
+  {
+    "revision": "157ec87d629157e36b82b027468a66e0",
+    "url": "/img/implementation.jpg"
+  },
+  {
+    "revision": "83eed583097b1601f53ac70fcc35c636",
+    "url": "/img/lean-ux-principles-cropped.png"
+  },
+  {
+    "revision": "d69dee4f4247c31a067aaf3af1baa0fa",
+    "url": "/img/lean-ux-principles.png"
+  },
+  {
+    "revision": "8c7e067c893e373f6a20477dbc8bf0c3",
+    "url": "/img/lean-ux.jpeg"
+  },
+  {
+    "revision": "b0aae40b59ad176c4b254ceff2f94554",
+    "url": "/img/methods.png"
+  },
+  {
+    "revision": "8afe94bb2d21be6c479f7019c1260c01",
+    "url": "/img/obama.jpg"
+  },
+  {
+    "revision": "07a8fa8f8129742ea2d65536ad72ede2",
+    "url": "/img/paragraf.png"
+  },
+  {
+    "revision": "8f537fa4943811cce1de39c14a983991",
+    "url": "/img/persona-ben.png"
+  },
+  {
+    "revision": "f76220f0ebb80a1e5ba4f2ebb729b5db",
+    "url": "/img/personas.png"
+  },
+  {
+    "revision": "94467bead26d65b89f9d67eeddf77430",
+    "url": "/img/proces-diagram.png"
+  },
+  {
+    "revision": "d833d9ed20be94c842db7ed1bf4a063f",
+    "url": "/img/proces.png"
+  },
+  {
+    "revision": "213f5a7c9e95f8137b71ba27359df1f1",
+    "url": "/img/product-owner.jpg"
+  },
+  {
+    "revision": "74bee7d92d63756ddc3f0cfb5e016888",
+    "url": "/img/protopersonas.png"
+  },
+  {
+    "revision": "f1de03df5134729b81f08b82eecf5a3a",
+    "url": "/img/questions.png"
+  },
+  {
+    "revision": "5b4e9efc0e50853d40a2c6bec4f7d42f",
+    "url": "/img/scrum-framework.png"
+  },
+  {
+    "revision": "4220ad5972e8cee9666f1a0cfdb7beea",
+    "url": "/img/scrum-master.jpg"
+  },
+  {
+    "revision": "9e056a5d422e681579ef69845e502fce",
+    "url": "/img/system-developer.jpg"
+  },
+  {
+    "revision": "3fd534dc18253574cf8dfd9292c8a1ac",
+    "url": "/img/tools.png"
+  },
+  {
+    "revision": "31a1f177be01f90082fbd5d4b98edc4b",
+    "url": "/img/ui-designer.jpg"
+  },
+  {
+    "revision": "43306c93f9cd0cacf7dde50fb26f6191",
+    "url": "/img/usability-10-heuristics.png"
+  },
+  {
+    "revision": "fccec1ff757183153fd1e2025bc8c127",
+    "url": "/img/usability-interviews.png"
+  },
+  {
+    "revision": "eccec2e6da9c040907fcbe4babfd9443",
+    "url": "/img/usability-test-interview-script.png"
+  },
+  {
+    "revision": "26a83bce125238df34cc38c30aa11adc",
+    "url": "/img/usability-testing-flows.png"
+  },
+  {
+    "revision": "d60cb199bccf1586c8f0bff4539e53d5",
+    "url": "/img/usability.jpg"
+  },
+  {
+    "revision": "48cf1dc59962edbe917e63f4241d44ae",
+    "url": "/img/user-flows.png"
+  },
+  {
+    "revision": "b3f3acd89d88f13a742df1f62d6fb0e8",
+    "url": "/img/ux-products.png"
+  },
+  {
+    "revision": "1e418f9feb571612ed89d85c17065e78",
+    "url": "/img/webssm-protopersonaer/Slide1.png"
+  },
+  {
+    "revision": "c893e16dbdd89cfd0250dbf20c4a833e",
+    "url": "/img/webssm-protopersonaer/Slide2.png"
+  },
+  {
+    "revision": "dd573dd8185d849b781c5b67c8bfc5b3",
+    "url": "/img/webssm-protopersonaer/Slide3.png"
+  },
+  {
+    "revision": "3431aeff96b2dd256d5e8f208bc15138",
+    "url": "/img/webssm-protopersonaer/Slide4.png"
+  },
+  {
+    "revision": "c6e53a112f78d73701ec0d6b41cfce90",
+    "url": "/img/what.jpeg"
+  },
+  {
+    "revision": "b14d7f978517db76359e2b545bdbf2d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a84a38c017c10e086cf",
+    "url": "/js/about.e5e263d1.js"
+  },
+  {
+    "revision": "b5cdf6c638dfebcfeec3",
+    "url": "/js/app.34772b3d.js"
+  },
+  {
+    "revision": "2ad1c2592c0fdd2641eb",
+    "url": "/js/chunk-vendors.b5369247.js"
+  },
+  {
+    "revision": "ae99c36998ee29916a9d1d65d1b7312b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
